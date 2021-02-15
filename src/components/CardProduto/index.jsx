f8d9ddbd722910d74634df0a70c3086b11551703
Larissa Diniz <larissa.diniz_23@hotmail.com>
2021-02-15 20:59:09 -0300

    ajustar imagem ao card

--- src/components/CardProduto/index.jsx
+++ src/components/CardProduto/index.jsx
@@ -7,7 +7,7 @@ const Card = (props) => {
     return (
         <div className="card">
             <div id="img">
-                <img src={props.img} width="100%" height="auto" alt="imagem do produto"/>
+                <img src={props.img} alt="imagem do produto"/>
             </div>
             <h3>{props.nome}</h3>
             <p>{props.valor}</p>
